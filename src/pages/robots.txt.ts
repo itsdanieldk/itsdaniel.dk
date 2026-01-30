ff57029eb73a154fa0d11660ba3c4563ba5975ca
Daniel Larsen <dala@clever.dk>
2026-01-30 22:48:44 +0100

    Upgraded dependencies and fixed few bugs

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,14 @@
 import type { APIRoute } from "astro";
 
-const SITE_URL = "https://itsdaniel.dk/";
-
-const robotsTxt = `
+export const GET: APIRoute = ({ site }) => {
+  const baseUrl = site ?? "https://itsdaniel.dk/";
+  const robotsTxt = `
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL("sitemap-index.xml", SITE_URL).href}
+Sitemap: ${new URL("sitemap-index.xml", baseUrl).href}
 `.trim();
 
-export const GET: APIRoute = () => {
   return new Response(robotsTxt, {
     headers: {
       "Content-Type": "text/plain; charset=utf-8",
